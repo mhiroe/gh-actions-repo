855de1521fcd351c0f3f8f5f6884dbf939a81507
mitsuki.hiroe <mitsuki.hiroe@dena.com>
2021-08-03 16:32:02 +0900

    create gke self-hosted runner & try it

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ https://medium.com/interleap/automating-terraform-deployment-to-google-cloud-wit
 - gce multi instance group を使う
   https://registry.terraform.io/modules/terraform-google-modules/github-actions-runners/google/latest/submodules/gh-runner-mig-vm
 
+  - gce 動かす sa に 権限を付与してしまっている
+
 - k8s を使う
   https://tech.jxpress.net/entry/gitops-for-terraform-with-github-self-hosted-runner
   - https://github.com/bharathkkb/gh-runners/tree/master/gke
+
--- terraform/env/gh-actions/main.tf
+++ terraform/env/gh-actions/main.tf
@@ -36,7 +36,11 @@ resource "google_project_service" "enable-services" {
 
 # network settings
 resource "google_compute_network" "runner-vpc" {
-  name = "runner-vpc"
+  # auto_create_subnetworks         = false
+  delete_default_routes_on_create = false
+  name                            = "runner-vpc"
+  mtu                             = 0
+  routing_mode                    = "REGIONAL"
 }
 
 resource "google_compute_subnetwork" "runner-subnet" {
@@ -46,58 +50,31 @@ resource "google_compute_subnetwork" "runner-subnet" {
   region        = var.basic.region
 }
 
-# ## self-hosted-runner
+## self-hosted-runner
 
-# ## use module
-# ## https://registry.terraform.io/modules/terraform-google-modules/github-actions-runners/google/latest/submodules/gh-runner-mig-vm
-# module "github-actions-runners_gh-runner-mig-vm" {
-#   source  = "terraform-google-modules/github-actions-runners/google//modules/gh-runner-mig-vm"
-#   version = "1.0.0"
-#   # insert the 5 required variables here
-#   project_id = var.basic.project
-#   gh_token   = "ABAWB2RGEFSZBFDICNBGIQ3BAOMRO" # persolnal userd
-#   repo_name  = "gh-actions-repo"
-#   repo_owner = "mhiroe"
-#   repo_url   = "https://github.com/mhiroe/gh-actions-repo"
+## scratch runner
+resource "google_compute_instance" "gce-runner" {
+  name         = "gce-runner"
+  machine_type = "f1-micro"
+  zone         = var.basic.zone
 
-#   # optional variables
-#   service_account = "sa-tf-monyama@gh-actions-321300.iam.gserviceaccount.com"
-#   instance_name   = "runner-gce"
-#   machine_type    = "f1-micro"
-#   network         = google_compute_network.runner-vpc.name
-#   subnetwork      = google_compute_subnetwork.runner-subnet.name
-# }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20210720"
+    }
+  }
 
+  network_interface {
+    network    = google_compute_network.runner-vpc.name
+    subnetwork = google_compute_subnetwork.runner-subnet.name
+    access_config {}
+  }
 
-# ## make my self
-# resource "google_compute_instance" "gce-runner" {
-#   name         = "gce-runner"
-#   machine_type = "f1-micro"
-#   zone         = var.basic.region
+  metadata_startup_script = file("./runner.sh")
 
-#   boot_disk {
-#     initialize_params {
-#       image = "ubuntu-1804-bionic-v20210720"
-#     }
-#   }
+  metadata = {
+    "block-project-ssh-keys" = "true"
+    "sshKeys"                = var.ssh_keys
+  }
 
-#   metadata_startup_script = file("./init.sh")
-
-#   network_interface {
-#     subnetwork = "projects/[HostProjectName]/regions/us-west1/subnetworks/private-subnet01"
-#     access_config {
-#     }
-#   }
-
-#   network_interface {
-#     network    = "${google_compute_network.runner-vpc.name}"
-#     subnetwork = "${google_compute_subnetwork.runner-subnet.name}"
-#     access_config {}
-#   }
-
-#     metadata {
-#     "block-project-ssh-keys" = "true"
-#     "sshKeys" = "${var.ssh_keys}"
-#   }
-
-# }
+}
